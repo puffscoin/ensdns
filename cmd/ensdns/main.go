c4f032ded0ce8725f2adf170f56ae8d2fcca6d0c
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-05-13 14:24:29 -0300

    Update main.go

--- cmd/ensdns/main.go
+++ cmd/ensdns/main.go
@@ -1,11 +1,11 @@
 // Copyright 2016 Nick Johnson <arachnid@notdot.net>
 //
-// go-ethereum is free software: you can redistribute it and/or modify
+// go-puffscoin is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// go-ethereum is distributed in the hope that it will be useful,
+// go-PUFFScoin is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
