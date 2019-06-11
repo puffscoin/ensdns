9a7df0aae8bc53416124ec56f6c172f856c70e1f
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-06-11 12:28:09 -0300

    Update ens.go

--- ens/ens.go
+++ ens/ens.go
@@ -1,18 +1,3 @@
-// Copyright 2016 Nick Johnson <arachnid@notdot.net>
-//
-// go-ethereum is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// go-ethereum is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with go-ethereum. If not, see <http://www.gnu.org/licenses/>.
-
 //go:generate abigen --sol contract/ens.sol --pkg contract --out contract/ens.go
 
 package ens
